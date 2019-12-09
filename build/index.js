6a9f1b0588b85082ceee418d6607bc4b3b48b6c4
Jonathan Irwin <jonoirwinrsa@gmail.com>
2019-12-09 12:03:24 +0200

    chore: updates build

--- build/index.js
+++ build/index.js
@@ -1,51 +1,72 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+var __assign = function() {
+    __assign = Object.assign || function __assign(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
             s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
         }
         return t;
     };
     return __assign.apply(this, arguments);
 };
-import * as React from 'react';
-import { useSpring } from 'react-spring';
+
 var styles = {
+    overflow: 'visible',
     position: 'absolute',
     strokeLinecap: 'round',
     fill: 'none',
     strokeWidth: '30',
-    animation: 'dash 5s linear forwards'
+    transform: 'rotate(-90deg)',
+    transition: ' all 1s ease-in-out'
 };
-function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
-    return {
-        x: centerX + radius * Math.cos(angleInRadians),
-        y: centerY + radius * Math.sin(angleInRadians)
-    };
+/**
+ * Returns the end dash array value for each section of the chart
+ * @param radius - chart radius
+ * @param percentage - complete percentage
+ */
+function createDasharray(radius, percentage) {
+    if (!percentage) {
+        return '0 999';
+    }
+    var circumference = 2 * radius * Math.PI;
+    var percentToDraw = (percentage * circumference) / 100;
+    return percentToDraw + " 999";
 }
 /**
- * Generates a SVG arc path
- * @param x
- * @param y
- * @param radius
- * @param startAngle
- * @param endAngle
+ * Returns a simple Circle Chart using an SVG
+ * @param values - array of colors and percentages
+ * @param size - width and height of container
+ * @param radius - radius of the chart
+ * @constructor
  */
-function describeArc(x, y, radius, startAngle, endAngle) {
-    var start = polarToCartesian(x, y, radius, endAngle);
-    var end = polarToCartesian(x, y, radius, startAngle);
-    var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
-    return ['M', start.x, start.y, 'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y].join(' ');
-}
-export var CircleChart = function (_a) {
-    var values = _a.values, _b = _a.size, size = _b === void 0 ? 200 : _b;
-    // Sort so they are all visible
+var CircleChart = function (_a) {
+    var values = _a.values, _b = _a.size, size = _b === void 0 ? 200 : _b, _c = _a.radius, radius = _c === void 0 ? 100 : _c;
+    // Sort values so that all sections will be visible
     values.sort(function (a, b) { return b.angle - a.angle; });
-    var ann = useSpring({ x: 100, from: { x: 0 } });
-    return (React.createElement("div", { style: { margin: -size / 2 + 'px', position: 'absolute' } }, values.map(function (item, index) { return (React.createElement("svg", { style: __assign(__assign({}, styles), { height: size + 'px', width: size + 'px' }), stroke: item.color, key: index, strokeDashoffset: ann.x },
-        React.createElement("path", { d: describeArc(size / 2, size / 2, 94, 0, item.angle) }))); })));
+    return (React.createElement("div", { style: { position: 'absolute' } }, values.map(function (item) { return (React.createElement("svg", { key: item.color, strokeDasharray: createDasharray(radius, item.angle), style: __assign(__assign({}, styles), { marginTop: "-" + size + "px", height: size + "px", width: size + "px" }), stroke: item.color },
+        React.createElement("circle", { cx: '0', cy: '0', r: radius }))); })));
 };
-export default CircleChart;
+
+exports.CircleChart = CircleChart;
+exports.default = CircleChart;
 //# sourceMappingURL=index.js.map
